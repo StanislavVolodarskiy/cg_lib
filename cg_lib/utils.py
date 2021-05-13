b785ebebf880f1163dc07ec583145f105ecd988e
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2021-05-14 00:33:49 +0300

    add pairs

--- cg_lib/utils.py
+++ cg_lib/utils.py
@@ -1,3 +1,11 @@
+import itertools
+
+
+def pairs(seq):
+    it1, it2 = itertools.tee(seq)
+    next(it2, None)
+    return zip(it1, it2)
+
 
 def cyclic_pairs(seq):
     it = iter(seq)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,6 +3,15 @@ from six.moves import range, zip
 from cg_lib import utils
 
 
+def test_pairs():
+    assert list(utils.pairs([])) == []
+    assert list(utils.pairs([0])) == []
+    assert list(utils.pairs([0, 1])) == [(0, 1)]
+    assert list(utils.pairs([0, 1, 2])) == [(0, 1), (1, 2)]
+    assert list(utils.pairs(range(10))) == \
+        list(zip(range(9), list(range(1, 10))))
+
+
 def test_cyclic_pairs():
     assert list(utils.cyclic_pairs([])) == []
     assert list(utils.cyclic_pairs([0])) == [(0, 0)]
